a5dc61dfeac003c47b18ba73829801d19e74b70c
Samuel O'Malley <omalsa04@gmail.com>
2016-02-13 11:46:10 +1030

    Fixed ctrl-left and ctrl-right behaviour

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -21,8 +21,8 @@ setopt APPEND_HISTORY
 setopt COMPLETE_ALIASES
 
 # make terminal command navigation sane again
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
+bindkey '^[[1;5D' backward-word
+bindkey '^[[1;5C' forward-word
 bindkey '^[[5D' beginning-of-line
 bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
